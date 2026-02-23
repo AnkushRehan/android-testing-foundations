04c6fea7027e57752b84896e97cd696242282f55
anil rehan <anilr0984211@gmail.com>
2026-02-23 16:00:42 +0530

    testing android app

--- app/src/main/java/com/example/composepractice/feature/social/DetailScreen.kt
+++ app/src/main/java/com/example/composepractice/feature/social/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.composepractice.feature.social
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -54,6 +55,7 @@ import com.example.composeproject.data.networkSection.responseUtil.Status
 fun DetailScreen(navController: NavHostController) {
 
 
+    Log.d("TAG", "DetailScreen: ")
 
     val socialViewModel: SocialViewModel = hiltViewModel()
 
